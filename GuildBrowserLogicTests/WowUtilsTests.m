8d9bf73985886ff1f9ba29a4910830d03b457e01
huos3203 <724987481@qq.com>
2014-03-31 22:59:23 +0800

    添加添加逻辑测试 target ： GuildBrowserLogicTests

---
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -0,0 +1,34 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by huoshuguang on 14-3-31.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
+
+@implementation WowUtilsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
