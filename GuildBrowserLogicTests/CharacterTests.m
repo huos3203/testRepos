0932155a02b7269069d50bd488bd17f11af6d91e
huos3203 <724987481@qq.com>
2014-03-31 23:18:01 +0800

    通过本地的 JSON 数据来测试一个类

---
+++ GuildBrowserLogicTests/CharacterTests.m
@@ -0,0 +1,34 @@
+//
+//  CharacterTests.m
+//  GuildBrowser
+//
+//  Created by huoshuguang on 14-3-31.
+//  Copyright (c) 2014年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
+
+@implementation CharacterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
