084d2774719db47f37567f5acf4aa2250ea5d180
huos3203 <724987481@qq.com>
2014-03-31 23:38:30 +0800

    添加单元测试依赖文件

--- GuildBrowserLogicTests/WowUtilsTests.m
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -8,27 +8,61 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 
+#import "WoWUtils.h"
 @interface WowUtilsTests : SenTestCase
 
 @end
 
 @implementation WowUtilsTests
 
-- (void)setUp
+//- (void)setUp
+//{
+//    [super setUp];
+//    // Put setup code here; it will be run once, before the first test case.
+//}
+//
+//- (void)tearDown
+//{
+//    // Put teardown code here; it will be run once, after the last test case.
+//    [super tearDown];
+//}
+//
+//- (void)testExample
+//{
+//    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+//}
+
+
+// 1
+-(void)testCharacterClassNameLookup
 {
-    [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
+    // 2
+    STAssertEqualObjects(@"Warrior",
+                         [WoWUtils classFromCharacterType:1],
+                         @"ClassType should be Warrior");
+    // 3
+    STAssertFalse([@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2]],
+                  nil);
+    
+    // 4
+    STAssertTrue([@"Paladin" isEqualToString:[WoWUtils classFromCharacterType:2]],
+                 nil);
+    // add the rest as an exercise
 }
 
-- (void)tearDown
+- (void)testRaceTypeLookup
 {
-    // Put teardown code here; it will be run once, after the last test case.
-    [super tearDown];
+    STAssertEqualObjects(@"Human", [WoWUtils raceFromRaceType:1], nil);
+    STAssertEqualObjects(@"Orc", [WoWUtils raceFromRaceType:2], nil);
+    STAssertFalse([@"Night Elf" isEqualToString:[WoWUtils raceFromRaceType:45]],nil);
+    // add the rest as an exercise
 }
 
-- (void)testExample
+- (void)testQualityLookup
 {
-    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    STAssertEquals(@"Grey", [WoWUtils qualityFromQualityType:1], nil);
+    STAssertFalse([@"Purple" isEqualToString:[WoWUtils qualityFromQualityType:10]],nil);
+    // add the rest as an exercise
 }
 
 @end
